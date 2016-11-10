7906d7fc520ba62d85cdd58a4bc59df88eb015e9
stihlsicher <jahn@topographics.de>
2016-11-10 15:24:42 +0100

    KLASSE EINGEBUNDEN

--- src/com/esri/cordova/geolocation/controllers/GPSController.java
+++ src/com/esri/cordova/geolocation/controllers/GPSController.java
@@ -23,6 +23,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.location.OnNmeaMessageListener;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
